1a9789478282cecb53af66edb727b1b9ad426d39
Somiwoojoopapa1212 <shuttle1207@gmail.com>
2026-04-26 11:27:12 +0900

    feat: 구구단 게임 초기 구현 (핵심 게임 + PWA + 비둘기 캐릭터)

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'gugu-v1';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
